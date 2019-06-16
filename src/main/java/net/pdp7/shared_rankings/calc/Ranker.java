dedbfc2673e248b70810e1db07ebc80e37f3afb3
alex <alex@pdp7.net>
2019-06-16 21:36:37 +0200

    Reformat Ranker

--- src/main/java/net/pdp7/shared_rankings/calc/Ranker.java
+++ src/main/java/net/pdp7/shared_rankings/calc/Ranker.java
@@ -15,9 +15,9 @@ public class Ranker {
 		optionIndex.forEach((option, index) -> options[index] = option);
 		int[][] voteMatrix = calculateVoteMatrix(votes, optionIndex);
 		int[] wins = new int[voteMatrix.length];
-		for(int i=0; i<voteMatrix.length; i++) {
-			for(int j=i+1; j<voteMatrix.length; j++) {
-				if(voteMatrix[i][j] > voteMatrix[j][i]) {
+		for (int i = 0; i < voteMatrix.length; i++) {
+			for (int j = i + 1; j < voteMatrix.length; j++) {
+				if (voteMatrix[i][j] > voteMatrix[j][i]) {
 					wins[i]++;
 				}
 			}
@@ -33,13 +33,13 @@ public class Ranker {
 
 	protected <T> int[][] calculateVoteMatrix(List<List<List<T>>> votes, Map<T, Integer> optionIndex) {
 		int[][] voteMatrix = new int[optionIndex.size()][optionIndex.size()];
-		for(List<List<T>> vote : votes) {
-			for(int i=0; i<vote.size(); i++) {
+		for (List<List<T>> vote : votes) {
+			for (int i = 0; i < vote.size(); i++) {
 				List<T> winners = vote.get(i);
-				for(int j=i+1; j<vote.size(); j++) {
+				for (int j = i + 1; j < vote.size(); j++) {
 					List<T> losers = vote.get(j);
-					for(Object winner : winners) {
-						for(Object loser : losers) {
+					for (Object winner : winners) {
+						for (Object loser : losers) {
 							voteMatrix[optionIndex.get(winner)][optionIndex.get(loser)]++;
 						}
 					}
@@ -53,10 +53,10 @@ public class Ranker {
 		Map<T, Integer> optionIndex = new HashMap<T, Integer>();
 
 		int index = 0;
-		for(List<List<T>> vote : votes) {
-			for(List<T> rank : vote) {
-				for(T option : rank) {
-					if(optionIndex.containsKey(option)) {
+		for (List<List<T>> vote : votes) {
+			for (List<T> rank : vote) {
+				for (T option : rank) {
+					if (optionIndex.containsKey(option)) {
 						continue;
 					}
 					optionIndex.put(option, index++);
